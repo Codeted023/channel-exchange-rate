146aa7e7418e7501ae87f75eefb8eb4f92090cbe
Cristhian Pantoja <andrepantoja03@gmail.com>
2025-08-11 22:15:13 -0500

    feat: validator dni

--- src/main/java/com/pe/demo/service/impl/ExchangeRateServiceImpl.java
+++ src/main/java/com/pe/demo/service/impl/ExchangeRateServiceImpl.java
@@ -8,6 +8,7 @@ import com.pe.demo.service.ExchangeRateService;
 import com.pe.demo.utils.TooManyRequestsException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.BadRequestException;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 
@@ -27,6 +28,10 @@ public class ExchangeRateServiceImpl implements ExchangeRateService {
   @Override
   @Transactional
   public ResponseExchangeRate getExchangeRate(String dni) {
+    if (dni == null || dni.length() != 8) {
+      throw new BadRequestException("El DNI debe tener exactamente 8 caracteres");
+    }
+
     var dateNow = java.time.LocalDate.now().toString();
     if (exchangeRateRepository.countByDateAndDni(dateNow, dni) < 10) {
       var apiProxy = netApi.getExchangeRate();
